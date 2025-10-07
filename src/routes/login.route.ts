e52091250d401d008b4730f2f62c323b8ebe2691
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-10-07 22:25:05 +0530

    use a mooko return

--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -3,14 +3,23 @@ import { loginRouteSchema } from '../schemas/login.schema.js';
 
 export default async function loginRoutes(fastify: FastifyInstance) {
   fastify.post(
-    '/login',
+    '/test',
     {
       schema: loginRouteSchema,
       // preHandler: fastify.clerk.login()
     },
     async (request: FastifyRequest, reply: FastifyReply) => {
       console.log(request.body);
-      reply.send({ message: 'List of users' });
+      // mock response // loginResponseSchema
+      const response = {
+        accessToken: 'mocked-access-token',
+        refreshToken: 'mocked-refresh-token',
+        user: {
+          id: 'mocked-user-id',
+          email: 'mocked-user-email',
+        },
+      };
+      reply.send(response);
     }
   );
 }
