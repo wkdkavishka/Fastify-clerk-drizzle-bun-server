0f49bc145e345c9c032a0d4ef319e63dbc1c75a2
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-10-13 00:37:02 +0530

    add some , descriptions to how to set pool size

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -6,7 +6,7 @@ const db = drizzle({
   connection: {
     url: ENV.DATABASE_URL,
     ssl: DEV ? false : true,
-    max: 10, // max 10 connections in the pool
+    max: 10, // max 10 connections in the pool // num of cores * 2 , in the vps server // recomended
     idle_timeout: 10000, // release idle connections after 10s
   },
 });
